9b062b682c2c86e7481354b5fbb6e58fbe33ff63
Max <maxib2007@gmail.com>
2021-04-11 17:28:25 -0300

    changes on history

--- app/src/main/java/com/maxwell/lyricssearchapp/history/HistoryFragment.kt
+++ app/src/main/java/com/maxwell/lyricssearchapp/history/HistoryFragment.kt
@@ -1,27 +1,37 @@
-package com.maxwell.lyricssearchapp
+package com.maxwell.lyricssearchapp.history
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.maxwell.lyricssearchapp.LyricsViewModel
+import com.maxwell.lyricssearchapp.R
 
-class LastSearchesFragment : Fragment() {
+class HistoryFragment : Fragment() {
     private val lyricsViewModel: LyricsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_last_searches, container, false)
+        return inflater.inflate(R.layout.fragment_history, container, false)
     }
 
     override fun onViewCreated(v: View, savedInstanceState: Bundle?) {
         super.onViewCreated(v, savedInstanceState)
 
-        val rvLastSearches: RecyclerView = v.findViewById(R.id.rvLastSearches)
+        val rvHistory: RecyclerView = v.findViewById(R.id.rvHistory)
+        val adapter = HistoryAdapter(arrayListOf())
+        rvHistory.adapter = adapter
+        rvHistory.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+
+        lyricsViewModel.searchHistoryList.observe(viewLifecycleOwner, {
+            adapter.setData(it)
+        })
     }
 }
